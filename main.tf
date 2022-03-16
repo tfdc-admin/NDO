8cc89eb575d9cb230c79c9f7942e78773ce08418
tfdc-admin <101254171+tfdc-admin@users.noreply.github.com>
2022-03-16 14:29:43 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    mso = {
+      source = "CiscoDevNet/mso"
+    }
+  }
+}
+provider "mso" {
+    # cisco-mso user name
+    username = var.nd_username
+    # cisco-mso password
+    password = var.nd_password
+    # cisco-mso url
+    url      = var.nd_url
+    platform = var.platform
+}
