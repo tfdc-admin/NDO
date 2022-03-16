ef942bcf1b850066eb3c0e9279960db712e8213a
tfdc-admin <101254171+tfdc-admin@users.noreply.github.com>
2022-03-16 14:41:08 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,6 +19,22 @@ data "mso_user" "user" {
   username = "admin"
 }
 
+data "mso_site" "Site01" {
+  name  = "ACI-01"
+}
+
+data "mso_site" "Site02" {
+  name  = "ACI-02"
+}
+
 output "user" {
   value = data.mso_user.user
+}
+
+output "Site01" {
+  value = data.mso_site.Site01
+}
+
+output "Site02" {
+  value = data.mso_site.Site02
 }
