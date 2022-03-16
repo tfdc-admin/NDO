8510e405a8cc345cc2f2f8e8eb77fe45b2a323f0
tfdc-admin <101254171+tfdc-admin@users.noreply.github.com>
2022-03-16 14:33:42 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "nd_username" {
+    type = string
+}
+
+variable "nd_password" {
+    type = string
+}
+
+variable "nd_url" {
+    type = string
+}
+
+variable "platform" {
+    type = string
+}
+
+variable "TF_CLI_ARGS_apply" {
+    type = string
+}
+
+variable "TF_CLI_ARGS_plan" {
+    type = string
+}
+
+variable "TFE_PARALLELISM" {
+    type = number
+}
+
